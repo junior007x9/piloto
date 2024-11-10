9fb55aa4d98f2b015562ddb723f5535cc9bb7073
junior007x9 <santos.junior12@hotmail.com>
2024-11-10 20:52:25 -0300

    att

--- home/urls.py
+++ home/urls.py
@@ -1,10 +1,9 @@
+# urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="index"),
-    path('home/', views.home, name="home"),
-    path('sobre/', views.sobre, name="sobre"),
-    path('contato/', views.contato, name="entre_em_contato"),
-    path('item/<int:id>/', views.exibir_item, name="exibir_item"),
+    path('', views.index, name="index"),                         # Página inicial
+    path('exibir_item/<int:id>/', views.exibir_item, name="exibir_item"),  # Exibir item específico
+    path('dia_semana/<int:dia>/', views.dia_semana, name="dia_semana"),    # Exibir dia da semana
 ]
--- home/views.py
+++ home/views.py
@@ -1,16 +1,32 @@
+# views.py
 from django.shortcuts import render
 
+# Página inicial que usa o template base
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'base.html')
 
-def home(request):
-    return render(request, 'home.html')
+# views.py
+from django.shortcuts import render
 
-def sobre(request):
-    return render(request, 'sobre.html')
-
-def contato(request):
-    return render(request, 'contato.html')
-
+# Página que exibe um item específico
 def exibir_item(request, id):
-    return render(request, 'exibir_item.html', {'id': id})
+    return render(request, 'id.html', {
+        'id': id  # Passa o ID como variável para o template
+    })
+
+# Página que exibe o dia da semana
+def dia_semana(request, dia):
+    dias_semana = {
+        1: "Domingo",
+        2: "Segunda-feira",
+        3: "Terça-feira",
+        4: "Quarta-feira",
+        5: "Quinta-feira",
+        6: "Sexta-feira",
+        7: "Sábado"
+    }
+    dia_nome = dias_semana.get(dia, "Dia inválido")
+    return render(request, 'dia_semana.html', {
+        'dia': dia,          # Passa o número do dia
+        'dia_nome': dia_nome # Passa o nome do dia da semana
+    })
