97cadd79d91826c78cc05557813e7d270c7ebfad
junior007x9 <santos.junior12@hotmail.com>
2024-11-26 22:15:59 -0300

    att

--- home/models.py
+++ home/models.py
@@ -1,3 +1,3 @@
-import locale
 from django.db import models
 
+# Create your models here.
--- utils/context_processors.py
+++
@@ -1,8 +0,0 @@
-# utils/context_processors.py
-from datetime import datetime
-
-def data_atual(request):
-    return {
-        'data_atual': datetime.now()
-    }
-    
